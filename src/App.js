a46fa7ed4d946ea3cb63fbd0565b360bd1da461e
Adrian Delpha <adrian.delpha@gmail.com>
2019-06-26 08:26:25 -0700

    Jumbotron home grown

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 const krazyKeetGames = (props) =>{
   return(
     <div className="App">
+      <AnimateBanner games={props.games}/>
       <NavBar games={props.games}/>
       <ul>{gameArray(props)}</ul>
     </div>
@@ -16,8 +17,15 @@ const gameArray = (props) => {
         <img src={item.background}></img>
         <div className="text_box">
           <h2>{item.name}</h2>
-          <span>{item.version}</span>
-          <span>{item.description}</span>
+          <p>{item.version}</p>
+          <p>{item.description}</p>
+          <p>{item.details}</p>
+          <img src={item.media[0]}></img>
+          <img src={item.media[1]}></img>
+          <img src={item.media[2]}></img>
+          <a target="_blank" href={item.youtube}>Trailer</a>
+          <a target="_blank" href={item.download}>Download</a>
+          <a target="_blank" href={item.gitHub}>Source</a>
         </div>
       </li>
     )
@@ -28,10 +36,30 @@ const navigate = (i) =>{
   document.getElementById(i).scrollIntoView();
 }
 
-const titleBanner = (props) => {
-  return(
-    <h1>Krazy Keet Games LLC</h1>
-  )
+const timerBanner = (me) => {
+  setTimeout(()=>{
+    me.setState({banner : me.props.games[Math.floor(Math.random() * me.props.games.length)].background});
+    timerBanner(me);
+  }, 6000);
+}
+
+class AnimateBanner extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {banner: "../art/annexback.jpg"}
+  }
+  componentDidMount(props){
+    timerBanner(this);
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>Krazy Keet Games LLC</h1>
+        <img src={this.state.banner} alt="krazyKeetGames"/>
+      </div>
+    )
+  }
 }
 
 const NavBar = (props) => {
@@ -41,7 +69,7 @@ const NavBar = (props) => {
         {
           props.games.map((item, index)=>
             <li key={index} onClick={() => navigate(index)}>
-              <img src={item.thumb}/>
+              <img src={item.thumb} alt={item.name}/>
             </li>
           )
         }
