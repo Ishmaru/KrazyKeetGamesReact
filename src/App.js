db44a6f838b2443db5782ba1d39f4cb18a20fd10
Adrian Delpha <adrian.delpha@gmail.com>
2019-06-27 08:01:51 -0700

    ann functionality

--- src/App.js
+++ src/App.js
@@ -3,29 +3,36 @@ import React, { Component } from 'react';
 const krazyKeetGames = (props) =>{
   return(
     <div className="App">
+      <h1>Krazy Keet Games LLC</h1>
       <AnimateBanner games={props.games}/>
       <NavBar games={props.games}/>
-      <ul>{gameArray(props)}</ul>
+      <div className="container">
+        <ul>{gameArray(props)}</ul>
+      </div>
     </div>
   );
 }
-
+       // <img className="game_main" src={item.background}></img>
 const gameArray = (props) => {
   return (
     props.games.map((item, index)=>
-      <li className="game_graphic" id={index} key={index}>
-        <img src={item.background}></img>
+      <li className="game" id={index} key={index}>
+        <div className="game_main">
+          <img src={item.background}></img>
+        </div>
         <div className="text_box">
           <h2>{item.name}</h2>
           <p>{item.version}</p>
           <p>{item.description}</p>
           <p>{item.details}</p>
-          <img src={item.media[0]}></img>
-          <img src={item.media[1]}></img>
-          <img src={item.media[2]}></img>
-          <a target="_blank" href={item.youtube}>Trailer</a>
-          <a target="_blank" href={item.download}>Download</a>
-          <a target="_blank" href={item.gitHub}>Source</a>
+          <div className="media flex">
+            <img src={item.media[0]}></img>
+            <img src={item.media[1]}></img>
+            <img src={item.media[2]}></img>
+          </div>
+          <div className="button" target="_blank" href={item.youtube}>Trailer</div>
+          <div className="button" target="_blank" href={item.download}>Download</div>
+          <div className="button" target="_blank" href={item.gitHub}>Source</div>
         </div>
       </li>
     )
@@ -36,26 +43,24 @@ const navigate = (i) =>{
   document.getElementById(i).scrollIntoView();
 }
 
-const timerBanner = (me) => {
-  setTimeout(()=>{
-    me.setState({banner : me.props.games[Math.floor(Math.random() * me.props.games.length)].background});
-    timerBanner(me);
-  }, 6000);
-}
-
 class AnimateBanner extends React.Component{
   constructor(props){
     super(props);
-    this.state = {banner: "../art/annexback.jpg"}
+    this.state = {banner: "../art/annexback.jpg", index: 0}
   }
   componentDidMount(props){
-    timerBanner(this);
+    setInterval(()=>{
+      let indexvalue = Math.floor(Math.random() * this.props.games.length);
+      this.setState({
+        banner : this.props.games[indexvalue].background,
+        index: indexvalue
+      });
+    }, 7000);
   }
 
   render() {
     return(
-      <div>
-        <h1>Krazy Keet Games LLC</h1>
+      <div className="jumbotron" onClick={() => navigate(this.state.index)}>
         <img src={this.state.banner} alt="krazyKeetGames"/>
       </div>
     )
@@ -64,7 +69,7 @@ class AnimateBanner extends React.Component{
 
 const NavBar = (props) => {
   return (
-    <nav className="container">
+    <nav className="navbar flex">
       <ul>
         {
           props.games.map((item, index)=>
@@ -78,5 +83,32 @@ const NavBar = (props) => {
   );
 }
 
+const NewsList = (props) => {
+  return (
+    props.map((item, index)=>
+      <li>
+        <p>{item.body}</p>
+      </li>
+    )
+  )
+}
+
+
+class NewsSection extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      news: []
+    }
+  }
+  render(){
+    return(
+      <div>
+        <h2>News</h2>
+        <ul>{NewsList(this.state.news)}</ul>
+      </div>
+    )
+  }
+}
 
 export default krazyKeetGames;
